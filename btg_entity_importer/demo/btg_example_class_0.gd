8be9f994046fc464de5271836717334e08d7d00a
Nathan Zimmer <96386265+NathanZimmer@users.noreply.github.com>
2025-03-04 20:04:12 -0500

    formatted and linted gdscript

--- btg_entity_importer/demo/btg_example_class_0.gd
+++ btg_entity_importer/demo/btg_example_class_0.gd
@@ -1,7 +1,7 @@
 ## This is an example object for the BTG pipeline
 class_name BTGExampleClass0 extends Node3D
 
-enum ExampleEnum {OPTION_1, OPTION_2, OPTION_3}
+enum ExampleEnum { OPTION_1, OPTION_2, OPTION_3 }
 
 @export var string_example: String = "string example"
 @export var int_example: int = 1
