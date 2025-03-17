c37e288ba527d83c2bbfc3cbd8a64e6697a605b1
Nathan Zimmer <96386265+NathanZimmer@users.noreply.github.com>
2025-03-16 21:45:53 -0400

    renamed files and classes, added docstring

--- btg_entity_importer/src/config.gd
+++ btg_entity_importer/src/config.gd
@@ -1,21 +1,21 @@
 @tool
 extends EditorPlugin
 
-var btg_post_import_plugin
+var btg_importer
 
 
 func _enter_tree() -> void:
     add_custom_type(
-        "BTGImporter",
+        "BTGImporterOld",
         "Node3D",
         preload("uid://c8aafnscexbny"),
         preload("uid://cq0fqc6flo66b"),
     )
-    btg_post_import_plugin = preload("uid://bu1bg70u1dtyq").new()
-    add_scene_post_import_plugin(btg_post_import_plugin)
+    btg_importer = preload("uid://bu1bg70u1dtyq").new()
+    add_scene_post_import_plugin(btg_importer)
 
 
 func _exit_tree() -> void:
-    remove_custom_type("BTGImporter")
-    remove_scene_post_import_plugin(btg_post_import_plugin)
-    btg_post_import_plugin = null
+    remove_custom_type("BTGImporterOld")
+    remove_scene_post_import_plugin(btg_importer)
+    btg_importer = null
