ac0d53f2650ed6c31e774e0afdb574a691820f85
Nathan Zimmer <96386265+NathanZimmer@users.noreply.github.com>
2025-03-16 22:09:59 -0400

    deleted old importer (#19)

--- btg_entity_importer/src/config.gd
+++ btg_entity_importer/src/config.gd
@@ -5,17 +5,10 @@ var btg_importer
 
 
 func _enter_tree() -> void:
-    add_custom_type(
-        "BTGImporterOld",
-        "Node3D",
-        preload("uid://c8aafnscexbny"),
-        preload("uid://cq0fqc6flo66b"),
-    )
     btg_importer = preload("uid://bu1bg70u1dtyq").new()
     add_scene_post_import_plugin(btg_importer)
 
 
 func _exit_tree() -> void:
-    remove_custom_type("BTGImporterOld")
     remove_scene_post_import_plugin(btg_importer)
     btg_importer = null
